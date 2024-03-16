7ef986f9e6cdaea72325bc671b1c43af7bb5501f
walid <s3972717@student.rmit.edu.au>
2024-03-16 17:25:22 +1100

    Plan and some css styling

---
+++ myPlan/plan.js
@@ -0,0 +1,16 @@
+// if user clicks box - add X and O alternately
+//once user 1 clicks 3 boxes - check if in row
+//once user 2 clicks 3 boxes - check if in row
+// check if row complete every click until 9 clicks
+// declare tie if no rows after 9 click
+//check if win by using if statement - e.g if box 1 && 2 && 3 = selected else if box 4 && 5 && 6 = selected else if box 7 && 8 && 9 = selected else if "do the same for diagonal & horizontal"
+// if tie or winner declared - winner message. update score with winner. clear board. option to play again
+//
+
+// Steps:
+// HTML - complete data required for game.
+// CSS - make squares big enough + borders
+// JS - clicks/event listeners, declare winner/tie
+// Start page, Options, Select Player, game screen, (maybe add a winner screen with animation. maybe intro animation by characters)
+
+// Mortal Kombat theme
